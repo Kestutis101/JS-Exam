c18292378df54d5c423f2b383621914a1484b37d
Kestutis101 <miceikakestutis@gmail.com>
2023-01-30 18:45:07 +0200

    Task 1 done

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,22 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: rezultatas turi būti matomas pateikus formą ir atvaizduojamas
 <div id="output"></div> viduje. Gautus atsakymus stilizuokite naudojant CSS;
 ------------------------------------------------------------------- */
+
+const userInput = document.querySelector("#search");
+
+document.querySelector("#submit-btn").addEventListener("click", (e) => {
+  e.preventDefault();
+  const user = userInput.value;
+  const parLb = document.createElement("p");
+  const parG = document.createElement("p");
+  const parOz = document.createElement("p");
+  const output = document.querySelector("#output");
+
+  parLb.textContent = `Svoris svarais (lb) : ${user * 2.2046}`;
+
+  parG.textContent = `Svoris gramais (g) : ${user / 0.001}`;
+
+  parOz.textContent = `Svoris uncijomis (oz) : ${user * 35.274}`;
+
+  output.append(parLb, parG, parOz);
+});
