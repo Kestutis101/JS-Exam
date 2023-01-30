8898e0eb87104236f1badd11565a17a5c44bdbd8
Kestutis101 <miceikakestutis@gmail.com>
2023-01-30 21:32:14 +0200

    Task 5 done

--- Task 5/script.js
+++ Task 5/script.js
@@ -9,13 +9,36 @@ atveju grąžins masyvą su "users", kurie yra pilnamečiai.
 -------------------------------------------------------------------------- */
 
 const users = [
-  { id: '1', name: 'John Smith', age: 20, hasDog: true },
-  { id: '2', name: 'Ann Smith', age: 24, hasDog: false },
-  { id: '3', name: 'Tom Jones', age: 31, hasDog: true },
-  { id: '4', name: 'Rose Peterson', age: 17, hasDog: false },
-  { id: '5', name: 'Alex John', age: 25, hasDog: true },
-  { id: '6', name: 'Ronald Jones', age: 63, hasDog: true },
-  { id: '7', name: 'Elton Smith', age: 16, hasDog: true },
-  { id: '8', name: 'Simon Peterson', age: 30, hasDog: false },
-  { id: '9', name: 'Daniel Cane', age: 51, hasDog: true },
+  { id: "1", name: "John Smith", age: 20, hasDog: true },
+  { id: "2", name: "Ann Smith", age: 24, hasDog: false },
+  { id: "3", name: "Tom Jones", age: 31, hasDog: true },
+  { id: "4", name: "Rose Peterson", age: 17, hasDog: false },
+  { id: "5", name: "Alex John", age: 25, hasDog: true },
+  { id: "6", name: "Ronald Jones", age: 63, hasDog: true },
+  { id: "7", name: "Elton Smith", age: 16, hasDog: true },
+  { id: "8", name: "Simon Peterson", age: 30, hasDog: false },
+  { id: "9", name: "Daniel Cane", age: 51, hasDog: true },
 ];
+
+const userNamesWhoHasDog = document.createElement("p");
+const userNamesWhoIsAdult = document.createElement("p");
+
+function filterDogOwners(users) {
+  const filteredArray = users
+    .filter((user) => (user.hasDog === true ? user.name : false))
+    .map((user) => ` ${user.name}`);
+  userNamesWhoHasDog.textContent = `THEY HAS DOG: ${filteredArray}`;
+  document.body.append(userNamesWhoHasDog);
+}
+
+filterDogOwners(users);
+
+function filterAdults(users) {
+  const adults = users
+    .filter((user) => (user.age >= 18 ? user.name : false))
+    .map((user) => ` ${user.name}`);
+  userNamesWhoIsAdult.textContent = `THEY ARE ADULTS: ${adults}`;
+  document.body.append(userNamesWhoIsAdult);
+}
+
+filterAdults(users);
