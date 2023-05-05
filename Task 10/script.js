3463a4a06ea445ff4e3b2a3f876034a2beb43518
Kestutis101 <miceikakestutis@gmail.com>
2023-05-05 22:46:13 +0300

    fixed grammar on function name

--- Task 10/script.js
+++ Task 10/script.js
@@ -5,12 +5,12 @@ Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pat
 import { composition } from "./modules/math/composition.js";
 import { division } from "./modules/math/division.js";
 import { multiplication } from "./modules/math/multiplication.js";
-import { substraction } from "./modules/math/subtraction.js";
+import { subtraction } from "./modules/math/subtraction.js";
 import { one, two, three, four, five } from "./modules/numbers/numbers.js";
 
 let a = composition(one, four);
 let b = division(four, two);
-let c = substraction(three, two);
+let c = subtraction(three, two);
 let d = multiplication(five, two);
 
 console.log(a);
